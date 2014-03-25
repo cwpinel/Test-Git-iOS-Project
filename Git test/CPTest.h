89aee99e63aa2f2f45b3baa3ec807093539fd00a
Christopher Pinel <cwpinel@gmail.com>
2014-03-24 19:49:11 -0600

    added a test class

---
+++ Git test/CPTest.h
@@ -0,0 +1,13 @@
+//
+//  CPTest.h
+//  Git test
+//
+//  Created by Christopher Pinel on 2014-03-24.
+//  Copyright (c) 2014 Christopher Pinel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CPTest : NSObject
+
+@end
---
+++ Git test/CPTest.m
@@ -0,0 +1,13 @@
+//
+//  CPTest.m
+//  Git test
+//
+//  Created by Christopher Pinel on 2014-03-24.
+//  Copyright (c) 2014 Christopher Pinel. All rights reserved.
+//
+
+#import "CPTest.h"
+
+@implementation CPTest
+
+@end
